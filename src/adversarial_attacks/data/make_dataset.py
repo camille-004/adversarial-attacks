9a7881880566e5937b6b52de07c3ad63d695484c
camille-004 <dunningcamille@gmail.com>
2022-05-15 01:26:39 -0700

    Preprocess and save data

--- src/adversarial_attacks/data/make_dataset.py
+++ src/adversarial_attacks/data/make_dataset.py
@@ -37,15 +37,75 @@ def unpickle_cifar(batch_id: int) -> Tuple[np.ndarray, List]:
     return features, labels
 
 
+def normalize(x: np.ndarray) -> np.ndarray:
+    """
+    Normalize input images.
+
+    :param x: NumPy array of input images (each image is [32, 32, 3])
+    :return: Normalized image
+    """
+    return (x - np.min(x)) / (x - np.max(x))
+
+
+def one_hot_encode(x: List) -> np.ndarray:
+    """
+    One-hot-encode a list of labels
+
+    :param x: Input list of labels from one batch
+    :return: One-hot-encoded NumPy array
+    """
+    encoded = np.zeros((len(x), config["n_labels"]))
+
+    for idx, val in enumerate(x):
+        encoded[idx][val] = 1
+
+    return encoded
+
+
+def preprocess_data(n_batches: int) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Load n_batches batches, normalize features, and one-hot-encode labels.
+
+    :param n_batches: Number of batches to load
+    :return: Preprocessed features and labels
+    """
+    assert n_batches <= config["n_batches"]
+    features = []
+    labels = []
+
+    for i in range(1, n_batches + 1):
+        feature_batch, label_batch = unpickle_cifar(i)
+        features.extend(feature_batch)
+        labels.extend(label_batch)
+
+    features = normalize(np.array(features))
+    labels = one_hot_encode(labels)
+
+    return features, labels
+
+
+def save_data(features: np.ndarray, labels: np.ndarray, path: str) -> None:
+    """
+    Save features and labels as a pickle file.
+
+    :param features: Features to save.
+    :param labels: One-hot-encoded labels to save.
+    :param path: Output save path.
+    :return:
+    """
+    assert labels.shape[1] == config["n_labels"]
+    pickle.dump((features, labels), open(path, "wb"))
+
+
 @click.command()
-@click.argument("batch_id", type=click.INT)
-@click.argument("output_filepath", type=click.Path())
+@click.argument("n_batches", type=click.INT)
+@click.argument("output_filename", type=click.Path())
 def main(batch_id: int, output_filepath: str) -> None:
     """
     Run data processing scripts to turn raw data into processed data to be saved in processed data folder
 
-    :param batch_id: CIFAR-10 batch
-    :param output_filepath: File path in which to save processed data
+    :param batch_id: Number of batches to load
+    :param output_filepath: Name of file to save in processed folder
     :return:
     """
     log.info("Making final dataset from raw data")
